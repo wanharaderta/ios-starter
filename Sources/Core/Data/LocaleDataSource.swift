a024071dc5b8f2153cee51c408e0c70ec8ff6068
wanhar <wanhardaengmari@gmail.com>
2021-05-09 16:47:06 +0700

    adding example

--- Sources/Core/Data/LocaleDataSource.swift
+++ Sources/Core/Data/LocaleDataSource.swift
@@ -4,6 +4,15 @@
 //
 //  Created by Wanhar on 24/02/21.
 //
+/*
+  example
+   func list(request: Request?) -> AnyPublisher<[Response], Error>
+   func add(entities: [Response]) -> AnyPublisher<Bool, Error>
+   func add(entity: Response) -> AnyPublisher<Bool, Error>
+   func get(itemId: String) -> AnyPublisher<Response, Error>
+   func update(itemId: Int, entity: Response) -> AnyPublisher<Bool, Error>
+   func delete(entity: Response) -> AnyPublisher<Bool, Error>
+ */
 
 import Foundation
 import Combine
@@ -11,11 +20,4 @@ import Combine
 public protocol LocaleDataSource {
   associatedtype Request
   associatedtype Response
-  
-//  func list(request: Request?) -> AnyPublisher<[Response], Error>
-//  func add(entities: [Response]) -> AnyPublisher<Bool, Error>
-//  func add(entity: Response) -> AnyPublisher<Bool, Error>
-//  func get(itemId: String) -> AnyPublisher<Response, Error>
-//  func update(itemId: Int, entity: Response) -> AnyPublisher<Bool, Error>
-//  func delete(entity: Response) -> AnyPublisher<Bool, Error>
 }
